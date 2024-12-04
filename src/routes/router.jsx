f9e8d66b1c5d9388b3670e04ab788208a7cc1838
adnan <sirgeant10@gmail.com>
2024-12-04 22:00:47 +0600

    Add signin and signup routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import SignIn from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
     loader: () => fetch("/api/movies"),
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 
