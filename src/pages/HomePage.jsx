bacc1c77f830587f2635bec901134285b7d6e162
adnan <sirgeant10@gmail.com>
2024-12-04 21:36:17 +0600

    Add homepage route

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Banner from "../components/Banner";
+import Featured from "../components/Featured";
+import Navbar from "../components/Navbar";
+import Watch from "../components/Watch";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Watch />
+      <Featured />
+    </>
+  );
+};
+
+export default HomePage;
