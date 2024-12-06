648774a4e6962c0fd8da51f1ffe2b9abdf2a274b
adnan <sirgeant10@gmail.com>
2024-12-06 21:39:59 +0600

    Show loading screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import Banner from "../components/Banner";
 import Featured from "../components/Featured";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import PopCat from "../components/PopCat";
 import Watch from "../components/Watch";
+import { AuthContext } from "../provider/AuthProvider";
+import Loading from "../components/Loading";
 
 const HomePage = () => {
+  const { loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Navbar />
