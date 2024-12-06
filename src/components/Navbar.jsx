2bd798a9cb86afb7bec0716a5c3d736c8bc9e96d
adnan <sirgeant10@gmail.com>
2024-12-06 21:37:20 +0600

    Dynamically show btn or user image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { FaTv } from "react-icons/fa6";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { IoPersonCircle } from "react-icons/io5";
 
 const Navbar = () => {
   const [navBg, setNavBg] = useState(false);
   const { pathname } = useLocation();
+  const { user, logOut } = useContext(AuthContext);
 
   const changeNavBg = () => {
     window.scrollY >= 550 ? setNavBg(true) : setNavBg(false);
@@ -19,8 +22,7 @@ const Navbar = () => {
   }, []);
 
   const bg = navBg && "bg-base-300";
-  const headerStyles =
-    pathname === "/" ? "fixed top-0 z-50" : "bg-base-300";
+  const headerStyles = pathname === "/" ? "fixed top-0 z-50" : "bg-base-300";
 
   const links = (
     <>
@@ -47,16 +49,44 @@ const Navbar = () => {
     </>
   );
 
-  const logBtns = (
-    <div className="space-x-2">
-      <Link to="/signin" className="btn btn-primary">
-        Sign In
-      </Link>
-      <Link to="/signup" className="btn btn-secondary">
-        Sign Up
-      </Link>
-    </div>
-  );
+  const logBtns =
+    user && user.email ? (
+      <div className="flex gap-1 md:gap-4 items-center">
+        <div className="dropdown dropdown-hover dropdown-bottom dropdown-center mr-10">
+          <div tabIndex={0} role="button">
+            <div className="avatar flex place-items-center">
+              <div className="w-12 rounded-full">
+                {user && user?.photoURL ? (
+                  <img src={user.photoURL} alt="User Image" />
+                ) : (
+                  <IoPersonCircle />
+                )}
+              </div>
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-accent rounded-box z-[1] w-52 p-2 shadow"
+          >
+            <li className="text-center mb-2 font-semibold text-white">{user?.displayName}</li>
+            <li>
+              <button onClick={logOut} className="btn btn-sm btn-secondary">
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    ) : (
+      <div className="space-x-2">
+        <Link to="/signin" className="btn btn-primary">
+          Sign In
+        </Link>
+        <Link to="/signup" className="btn btn-secondary">
+          Sign Up
+        </Link>
+      </div>
+    );
 
   return (
     <header className={`${headerStyles} ${bg}  w-full`}>
