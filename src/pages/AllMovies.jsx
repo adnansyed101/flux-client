d388d726aad3d93a2de964939ad4757523820faf
adnan <sirgeant10@gmail.com>
2024-12-06 15:28:51 +0600

    Create Allmovies Page

---
+++ src/pages/AllMovies.jsx
@@ -0,0 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const AllMovies = () => {
+  const { data: movies } = useLoaderData();
+
+  return (
+    <>
+      <Navbar />
+      <div className="bg-accent py-10">
+        <div className="container mx-auto grid grid-cols-1 lg:grid-cols-5 gap-5">
+          {movies.map((movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AllMovies;
