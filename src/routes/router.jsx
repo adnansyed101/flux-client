274d2287ac3446e07529ec55d35efe47bb22eb7d
adnan <sirgeant10@gmail.com>
2024-12-06 15:28:33 +0600

    Add route to show all movies

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,12 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import SignIn from "../pages/Signin";
 import Signup from "../pages/Signup";
+import AllMovies from "../pages/AllMovies";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
     loader: () => fetch("/api/movies/featured"),
+  },
+  {
+    path: "/movies",
+    element: <AllMovies />,
+    loader: () => fetch("/api/movies"),
   },
   {
     path: "/signin",
