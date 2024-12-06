82554e4a911a226fc963015f9644fd3096fd787c
adnan <sirgeant10@gmail.com>
2024-12-06 21:41:11 +0600

    Add authentication

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,109 +1,119 @@
-// import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BsGoogle } from "react-icons/bs";
-// import { FaEye } from "react-icons/fa";
-// import { FaEyeSlash } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-// import { AuthContext } from "../provider/AuthProvider";
-// import { toast } from "react-toastify";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+import { toast } from "react-toastify";
 import "animate.css";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
-  //   const [showPwd, setShowPwd] = useState(false);
-  //   const [showVerifyPwd, setShowVerifyPwd] = useState(false);
-  //   const { createNewUser, setUser, updateUserProfile, createUserWithGoogle } =
-  //     useContext(AuthContext);
-  //   const navigate = useNavigate();
+  const [showPwd, setShowPwd] = useState(false);
+  const [showVerifyPwd, setShowVerifyPwd] = useState(false);
+  const { createNewUser, setUser, updateUserProfile, createUserWithGoogle } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    email: "",
+    password: "",
+  });
 
-  //   useEffect(() => {
-  //     document.title = "Go Travels | Login";
-  //   }, []);
+  useEffect(() => {
+    document.title = "Flux | Sign Up";
+  }, []);
 
-  //   const handleShowPwd = () => setShowPwd(!showPwd);
-  //   const handleShowVerifyPwd = () => setShowVerifyPwd(!showVerifyPwd);
+  const handleShowPwd = () => setShowPwd(!showPwd);
+  const handleShowVerifyPwd = () => setShowVerifyPwd(!showVerifyPwd);
 
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     const form = new FormData(e.target);
-  //     const name = form.get("name");
-  //     const photo = form.get("photoURL");
-  //     const email = form.get("email");
-  //     const password = form.get("password");
-  //     const verifyPassword = form.get("verifyPassword");
-  //     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+  const onSubmit = (data) => {
+    const { name, photoURL: photo, email, password, verifyPassword } = data;
 
-  //     if (password !== verifyPassword) {
-  //       toast.error("Passwords do not match");
-  //       return;
-  //     }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
-  //     if (!passwordRegex.test(password) || !passwordRegex.test(verifyPassword)) {
-  //       toast.warn("Must have 1 Uppercase, 1 Lowercase and atleast 6 character.");
-  //       return;
-  //     }
+    if (password !== verifyPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
 
-  //     createNewUser(email, password)
-  //       .then((result) => {
-  //         const user = result.user;
-  //         setUser(user);
-  //         updateUserProfile({ displayName: name, photoURL: photo })
-  //           .then(() => {
-  //             toast.success("Account Created Successfully.");
-  //             navigate("/");
-  //           })
-  //           .catch((error) => {
-  //             const errorCode = error.code;
-  //             toast.error(errorCode);
-  //           });
-  //       })
-  //       .catch((error) => {
-  //         const errorCode = error.code;
-  //         toast.error(errorCode);
-  //       });
-  //   };
+    if (!passwordRegex.test(password) || !passwordRegex.test(verifyPassword)) {
+      toast.warn("Must have 1 Uppercase, 1 Lowercase and atleast 6 character.");
+      return;
+    }
 
-  //   const handleGoogleSignUp = () => {
-  //     createUserWithGoogle()
-  //       .then((res) => {
-  //         const user = res.user;
-  //         setUser(user);
-  //         navigate("/");
-  //         toast.success("Account Created Successfully.");
-  //       })
-  //       .catch((err) => {
-  //         const errorCode = err.code;
-  //         toast.error(errorCode);
-  //       });
-  //   };
+    createNewUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        updateUserProfile({ displayName: name, photoURL: photo })
+          .then(() => {
+            toast.success("Account Created Successfully.");
+            navigate("/");
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            toast.error(errorCode);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        toast.error(errorCode);
+      });
+  };
+
+  const handleGoogleSignUp = () => {
+    createUserWithGoogle()
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+        navigate("/");
+        toast.success("Account Created Successfully.");
+      })
+      .catch((err) => {
+        const errorCode = err.code;
+        toast.error(errorCode);
+      });
+  };
 
   return (
-    <div className="hero bg-gradient-to-br from-midnightBlue to-slateGray min-h-screen text-white">
+    <div className="hero bg-gradient-to-br from-primary to-accent min-h-screen">
       <div className="hero-content">
-        <div className="card bg-primaryBg shadow-2xl animate__animated animate__bounceInUp">
+        <div className="card bg-base-100 shadow-2xl animate__animated animate__bounceInUp">
           <div className="card-body">
-            <form className="md:w-96">
+            <form className="md:w-96" onSubmit={handleSubmit(onSubmit)}>
               <h1 className="text-4xl md:text-5xl font-bold mb-2 text-center">
                 Register
               </h1>
               {/* Photo-URL */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Name</span>
+                  <span className="label-text">Name</span>
                 </label>
                 <input
+                  {...register("name", {
+                    required: "This is required",
+                  })}
                   type="text"
-                  name="name"
                   className="input border-accent"
-                  required
                 />
+                {errors.Name && (
+                  <p className="text-error">{errors.Name?.message}</p>
+                )}
               </div>
               {/* Photo-URL */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Photo URL</span>
+                  <span className="label-text">Photo URL</span>
                 </label>
                 <input
+                  {...register("photoURL", {
+                    required: "This is required",
+                  })}
                   type="text"
-                  name="photoURL"
                   className="input border-accent"
                   required
                 />
@@ -111,11 +121,13 @@ const Signup = () => {
               {/* Email */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Email</span>
+                  <span className="label-text">Email</span>
                 </label>
                 <input
+                  {...register("email", {
+                    required: "This is required",
+                  })}
                   type="email"
-                  name="email"
                   className="input border-accent"
                   required
                 />
@@ -123,60 +135,62 @@ const Signup = () => {
               {/* Password */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Password</span>
+                  <span className="label-text">Password</span>
                 </label>
                 <label className="input border-accent flex items-center gap-2">
                   <input
-                    // type={showPwd ? "text" : "password"}
-                    type="text"
-                    name="password"
+                    {...register("password", {
+                      required: "This is required",
+                    })}
+                    type={showPwd ? "text" : "password"}
                     className="grow"
                     autoComplete="off"
                     required
                   />
-                  {/* {showPwd ? (
+                  {showPwd ? (
                     <FaEyeSlash onClick={handleShowPwd} />
                   ) : (
                     <FaEye onClick={handleShowPwd} />
-                  )} */}
+                  )}
                 </label>
               </div>
               {/* Verify Password */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Verify Password</span>
+                  <span className="label-text">Verify Password</span>
                 </label>
                 <label className="input input-bordered flex items-center gap-2">
                   <input
-                    // type={showVerifyPwd ? "text" : "password"}
-                    type="text"
-                    name="verifyPassword"
+                    {...register("verifyPassword", {
+                      required: "This is required",
+                    })}
+                    type={showVerifyPwd ? "text" : "password"}
                     className="grow"
                     autoComplete="off"
                     required
                   />
-                  {/* {showVerifyPwd ? (
+                  {showVerifyPwd ? (
                     <FaEyeSlash onClick={handleShowVerifyPwd} />
                   ) : (
                     <FaEye onClick={handleShowVerifyPwd} />
-                  )} */}
+                  )}
                 </label>
               </div>
               <p className="mt-4">
                 Already have an account ?{" "}
-                <Link to="/login" className="text-accent">
-                  Login
+                <Link to="/signin" className="text-accent">
+                  Sign In
                 </Link>
               </p>
               <div className="form-control mt-4">
-                <button type="submit" className="primary-btn">
+                <button type="submit" className="btn btn-primary">
                   Register
                 </button>
               </div>
             </form>
             <button
               className="btn btn-outline btn-accent"
-              //   onClick={handleGoogleSignUp}
+              onClick={handleGoogleSignUp}
             >
               <BsGoogle /> Sign Up Using Google
             </button>
