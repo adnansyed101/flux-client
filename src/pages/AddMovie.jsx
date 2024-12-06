11e031e4b2468843d4a799579afc17b0fcdf2768
adnan <sirgeant10@gmail.com>
2024-12-06 15:56:17 +0600

    Create AddMovie page

---
+++ src/pages/AddMovie.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import MovieForm from "../components/MovieForm";
+import Navbar from "../components/Navbar";
+
+const AddMovie = () => {
+  return (
+    <>
+      <Navbar />
+      <MovieForm />
+      <Footer />
+    </>
+  );
+};
+
+export default AddMovie;
