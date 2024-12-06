82f67915c4cd6e05f70b3c1c44d5b758a52d85fc
adnan <sirgeant10@gmail.com>
2024-12-06 15:05:48 +0600

    Show featured and popCat sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import Banner from "../components/Banner";
 import Featured from "../components/Featured";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import PopCat from "../components/PopCat";
 import Watch from "../components/Watch";
 
 const HomePage = () => {
@@ -10,6 +12,8 @@ const HomePage = () => {
       <Banner />
       <Watch />
       <Featured />
+      <PopCat />
+      <Footer />
     </>
   );
 };
