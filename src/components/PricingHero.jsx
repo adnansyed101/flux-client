7de14c8cceffe5495a790fa537a6bdff80e02ca1
adnan <sirgeant10@gmail.com>
2024-12-07 22:13:48 +0600

    Create banner for pricing

---
+++ src/components/PricingHero.jsx
@@ -0,0 +1,15 @@
+const PricingHero = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Stream smarter, save more.</h1>
+          <p className="py-6">Get The Best Pricing From Here</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PricingHero;
