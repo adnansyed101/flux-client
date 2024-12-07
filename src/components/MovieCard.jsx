eb2346aaeb1d232f3f9ad2d26192736b1c476153
adnan <sirgeant10@gmail.com>
2024-12-07 18:21:59 +0600

    Change card styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,19 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
-  const { _id, imgLink, title, rating, duration } = movie;
+  const { _id, imgLink, title, rating, duration, genre } = movie;
   return (
-    <div className="card bg-base-100 shadow-xl">
+    <div className="card bg-base-100 image-full shadow-xl">
       <figure>
-        <img src={imgLink} alt="Movie" className="w-full"/>
+        <img src={imgLink} alt="Movie" className="w-full" />
       </figure>
-      <div className="card-body p-2">
-        <h2 className="card-title">{title}</h2>
-        <p>
-          Rating: {rating} <br /> Duration: {duration} mins
-        </p>
+      <div className="card-body justify-between">
+        <div className="space-y-4">
+          <h2 className="card-title text-2xl">{title}</h2>
+          <p className="text-xl">Rating: {rating}</p>
+          <p className="text-xl">Duration: {duration} mins</p>
+          <p className="text-xl">Genre: {genre}</p>
+        </div>
         <div className="card-actions">
           <Link to={`/movie/${_id}`} className="btn btn-primary">
             Details
