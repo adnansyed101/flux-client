4e2c6963a420cba7d5c5756ef59391b71b7fdf4f
adnan <sirgeant10@gmail.com>
2024-12-07 21:21:02 +0600

    Create update movie page

---
+++ src/pages/UpdateMoviePage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import UpdateMovieForm from "../components/UpdateMovieForm";
+
+const UpdateMoviePage = () => {
+  return (
+    <>
+      <Navbar />
+      <UpdateMovieForm />
+      <Footer />
+    </>
+  );
+};
+
+export default UpdateMoviePage;
