03d53d28ea8db0e248672e2d5caa4618109a9a2f
adnan <sirgeant10@gmail.com>
2025-01-07 16:20:32 +0600

    Change height of hero

--- src/components/PricingHero.jsx
+++ src/components/PricingHero.jsx
@@ -1,6 +1,6 @@
 const PricingHero = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-[calc(100vh-100px)]">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Stream smarter, save more.</h1>
