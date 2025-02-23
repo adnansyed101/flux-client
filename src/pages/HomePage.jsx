decadc7b9fce3736478ada05b69eb4ebe49e1dbf
Adnan <sirgeant10@gmail.com>
2025-02-23 21:02:32 +0600

    Remove nav and footer.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,20 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import Banner from "../components/Banner";
 import Featured from "../components/Featured";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import PopCat from "../components/PopCat";
 import Watch from "../components/Watch";
-import { AuthContext } from "../provider/AuthProvider";
-import Loading from "../components/Loading";
 
 const HomePage = () => {
-  const { loading } = useContext(AuthContext);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <>
-      <Navbar />
       <Banner />
       <Watch />
       <Featured />
       <PopCat />
-      <Footer />
     </>
   );
 };
